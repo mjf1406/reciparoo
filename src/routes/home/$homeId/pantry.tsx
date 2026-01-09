453614c2f280b13b6654d48e8a4c4506507d4ab5
Michael J. Fitzgerald <michael.fitzgerald.1406@gmail.com>
2026-01-10 08:58:36 +0900

    remove text

--- src/routes/home/$homeId/pantry.tsx
+++ src/routes/home/$homeId/pantry.tsx
@@ -61,12 +61,6 @@ function PantryPage() {
             />
             <div className="flex flex-col items-center justify-center min-h-[60vh] text-center">
                 <UnderConstructionImage />
-                <h2 className="text-2xl font-semibold mb-2">
-                    Under Construction
-                </h2>
-                <p className="text-muted-foreground">
-                    This feature is currently being developed. Check back soon!
-                </p>
             </div>
         </main>
     );
